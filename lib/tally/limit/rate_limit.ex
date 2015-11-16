b847b2f0f9f9f41cd82b80f6f89c5073ec1b36c6
Kevin Stone <kevinastone@gmail.com>
2015-11-16 07:37:19 -0800

    Separated rate limit header generation

--- lib/tally/limit/rate_limit.ex
+++ lib/tally/limit/rate_limit.ex
@@ -9,9 +9,15 @@ defmodule Tally.Limit.RateLimit do
   defp ttl(limit, :hour), do: ttl(limit, :min) * 60
   defp ttl(limit, :day), do: ttl(limit, :hour) * 24
 
-  def limit(conn, identity, limits \\ []) do
-    IO.inspect({:limits, limits})
+  defp limit_headers(limit, duration, used) do
+    %{
+      "x-ratelimit-limit" => to_string(limit),
+      "x-ratelimit-period" => Atom.to_string(duration),
+      "x-ratelimit-remaining" => to_string(max(limit - used, 0))
+    }
+  end
 
+  def limit(conn, identity, limits \\ []) do
     conn |> Plug.Conn.merge_resp_headers(%{"x-ratelimit-id" => identity})
 
     Enum.reduce limits, conn, fn {limit, duration}, conn ->
@@ -21,13 +27,7 @@ defmodule Tally.Limit.RateLimit do
         %ConCache.Item{value: 0, ttl: ttl(limit, duration)}
       end)
 
-      limit_headers = %{
-        "x-ratelimit-limit" => to_string(limit),
-        "x-ratelimit-period" => Atom.to_string(duration),
-        "x-ratelimit-remaining" => to_string(max(limit - used, 0))
-      }
-      conn = conn |> Plug.Conn.merge_resp_headers(limit_headers)
-      IO.inspect(limit_headers)
+      conn = conn |> Plug.Conn.merge_resp_headers(limit_headers(limit, duration, used))
 
       if used >= limit do
         conn = conn |> Plug.Conn.send_resp(429, "Too many requests") |> Plug.Conn.halt
