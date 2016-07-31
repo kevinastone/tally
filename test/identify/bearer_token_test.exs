d6bf3dc2fa1e223027b0fe93b947b4e32efa3162
Kevin Stone <kevinastone@gmail.com>
2016-07-31 14:58:44 -0700

    Added unit test for bearer token

---
+++ test/identify/bearer_token_test.exs
@@ -0,0 +1,29 @@
+defmodule Tally.Identify.BearerTokenTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  alias Tally.Identify.BearerToken
+
+  @header_name "authorization"
+
+  @opts BearerToken.init([])
+
+  test "Extracts Bearer Token" do
+    {:ok, _, identity} = conn(:get, "/hello")
+    |> put_req_header(@header_name, "Bearer abcd")
+    |> BearerToken.identify(@header_name)
+
+    assert identity == "abcd"
+  end
+
+  test "Error on Invalid Authorization Header" do
+    {:fail, _} =  conn(:get, "/hello")
+    |> put_req_header(@header_name, "abcd")
+    |> BearerToken.identify(@header_name)
+  end
+
+  test "Failure without Bearer Token" do
+    {:fail, _} =  conn(:get, "/hello")
+    |> BearerToken.identify(@header_name)
+  end
+end
